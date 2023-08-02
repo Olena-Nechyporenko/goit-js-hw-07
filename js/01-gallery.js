51eb287f1a04b6b5b485faef06c6f8ef975f129c
Olena-Nechyporenko <olenan0309@gmail.com>
2023-08-02 15:25:42 +0300

    finished 1 and 2 tasks

---
+++ js/01-gallery.js
@@ -0,0 +1,34 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+
+const galleryList = document.querySelector(".gallery")
+
+const markup = galleryItems.map( item => `<li class="gallery__item">
+<a class="gallery__link" href="${item.original}">
+  <img
+    class="gallery__image"
+    src="${item.preview}"
+    data-source="${item.original}"
+    alt="${item.description}"
+  />
+</a>
+</li>`).join("")
+
+galleryList.insertAdjacentHTML("beforeend", markup)
+
+galleryList.addEventListener("click", onClick)
+
+function onClick (evt) {
+evt.preventDefault()
+if(evt.target.nodeName !== "IMG") {
+  return
+}
+const instance = basicLightbox.create(`
+<img
+src="${evt.target.dataset.source}" width="800" height="600"
+/>
+`)
+instance.show()
+}
+
